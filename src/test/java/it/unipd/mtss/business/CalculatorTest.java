964895cf872c0d9aee0085d7e54ba3a9a1db5e7d
TommasoBerlaffa <83396379+TommasoBerlaffa@users.noreply.github.com>
2022-05-19 17:39:22 +0200

    Close #3; Aggiunta Test Primo Regalo

--- src/test/java/it/unipd/mtss/business/CalculatorTest.java
+++ src/test/java/it/unipd/mtss/business/CalculatorTest.java
@@ -31,15 +31,36 @@ public class CalculatorTest {
 	}
 	//If there are five or more processors, the price of cheapest one will be halved
 	@Test
-	public void Morethan5Processor_Test() throws BillException {
+	public void MoreThan5Processor_Test() throws BillException {
 		List<EItem> itemsOrdered = new ArrayList<EItem>();
         itemsOrdered.add(new EItem(itemType.Processor,"Processore1",1.0));
         itemsOrdered.add(new EItem(itemType.Processor,"Processore2",2.0));
         itemsOrdered.add(new EItem(itemType.Processor,"Processore3",3.0));
         itemsOrdered.add(new EItem(itemType.Processor,"Processore4",4.0));
         itemsOrdered.add(new EItem(itemType.Processor,"Processore5",5.0));
+        itemsOrdered.add(new EItem(itemType.Processor,"Processore6",0.5));
         User u=new User(4,"Gino", "Bob");
         double total = C.getOrderPrice(itemsOrdered,u);
-        assertEquals(14.5D, total, 0.0D);
+        assertEquals(15.25D, total, 0.0D);
 	}
+	
+	//If there are five or more processors, the price of cheapest one will be halved
+		@Test
+		public void MoreThan10Mouse_Test() throws BillException {
+			List<EItem> itemsOrdered = new ArrayList<EItem>();
+
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse1",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse2",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse3",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse4",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse5",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse6",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse7",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse8",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse9",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse0",1.0));
+	        User u=new User(4,"Gino", "Bob");
+	        double total = C.getOrderPrice(itemsOrdered,u);
+	        assertEquals(90.0D, total, 0.0D);
+		}
 }
