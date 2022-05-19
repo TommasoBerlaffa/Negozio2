44065c2859373586c78081c52918a35381fce2f9
Alcky26 <64201984+Alcky26@users.noreply.github.com>
2022-05-19 18:12:09 +0200

    Close #4;Aggiunta test feature SecondoRegalo

--- src/test/java/it/unipd/mtss/business/CalculatorTest.java
+++ src/test/java/it/unipd/mtss/business/CalculatorTest.java
@@ -27,7 +27,7 @@ public class CalculatorTest {
         itemsOrdered.add(new EItem(itemType.Processor,"Processore Bello",0.48));
         User u=new User(4,"Gino", "Bob");
         double total = C.getOrderPrice(itemsOrdered,u);
-        assertEquals(21.0D, total, 0.0D);
+        assertEquals(11.0D, total, 0.0D);
 	}
 	//If there are five or more processors, the price of cheapest one will be halved
 	@Test
@@ -44,7 +44,7 @@ public class CalculatorTest {
         assertEquals(15.25D, total, 0.0D);
 	}
 	
-	//If there are five or more processors, the price of cheapest one will be halved
+	//If there are ten or more mice, the price of cheapest one will set to 0
 		@Test
 		public void MoreThan10Mouse_Test() throws BillException {
 			List<EItem> itemsOrdered = new ArrayList<EItem>();
@@ -62,5 +62,64 @@ public class CalculatorTest {
 	        User u=new User(4,"Gino", "Bob");
 	        double total = C.getOrderPrice(itemsOrdered,u);
 	        assertEquals(90.0D, total, 0.0D);
+		}
+		
+		//If there are the same number of mice and keyboards, the cheapest one will set to 0 
+		//if the number of mice is more than 10, the cheapest mouse and keyboard will both be set to 0
+		@Test
+		public void EqualNumberOfMiceAndKeyboards_Test() throws BillException {
+			List<EItem> itemsOrdered = new ArrayList<EItem>();
+
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse1",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse2",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse3",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse4",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse5",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse6",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse7",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse8",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse9",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse10",1.0));
+
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard1",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard2",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard3",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard4",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard5",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard6",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard7",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard8",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard9",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard10",2.0));
+	        
+	        User u=new User(4,"Gino", "Bob");
+	        double total = C.getOrderPrice(itemsOrdered,u);
+	        assertEquals(180.0D, total, 0.0D);
+		}
+		
+		//If there are the same number of mice and keyboards, the cheapest one will set to 0
+		@Test
+		public void EqualNumberOfMiceAndKeyboardsLessThan10_Test() throws BillException {
+			List<EItem> itemsOrdered = new ArrayList<EItem>();
+
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse1",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse2",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse3",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse4",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse5",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse6",10.0));
+	        itemsOrdered.add(new EItem(itemType.Mouse,"Mouse10",2.0));
+
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard1",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard2",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard3",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard4",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard5",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard6",10.0));
+	        itemsOrdered.add(new EItem(itemType.Keyboard,"Keyboard10",1.0));
+	        
+	        User u=new User(4,"Gino", "Bob");
+	        double total = C.getOrderPrice(itemsOrdered,u);
+	        assertEquals(122.0D, total, 0.0D);
 		}
 }
