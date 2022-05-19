587355c269f5a939deac45a4559b36453310fa0b
Alcky26 <64201984+Alcky26@users.noreply.github.com>
2022-05-19 17:21:39 +0200

    Aggiunta primo sconto

--- src/main/java/it/unipd/mtss/business/Calculator.java
+++ src/main/java/it/unipd/mtss/business/Calculator.java
@@ -10,6 +10,7 @@ import java.util.List;
 import it.unipd.mtss.business.exception.BillException;
 import it.unipd.mtss.model.EItem;
 import it.unipd.mtss.model.User;
+import it.unipd.mtss.model.itemType;
 
 public class Calculator implements Bill {
 
@@ -17,9 +18,27 @@ public class Calculator implements Bill {
  public double getOrderPrice(List<EItem> itemsOrdered, User user) throws BillException
  {
   double total = 0.0;
+  int counter = 0;
+  double cost= 0.0;
   for(EItem i : itemsOrdered)
   {
    total += i.getPrice();
+   if(i.getType()==itemType.Processor)
+   {
+    counter++;
+    if(cost==0.0)
+    {
+     cost=i.getPrice();
+    }
+     else if(cost>i.getPrice())
+    {
+     cost=i.getPrice();
+    }
+   }
+  }
+  if(counter>=5)
+  {
+   total-=cost*0.5;
   }
   return total;
  }
